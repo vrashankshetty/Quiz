8434bacce35960ba8fbb31d2baef84b9aa4d7912
Vrashank Shetty <vrashankshetty.s@gmail.com>
2023-07-18 12:12:52 +0530

    other update

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,14 +5,19 @@ export default function Register() {
   const [name,setName]=useState('');
   const [password,setPassword]=useState('')
   const navigate=useNavigate();
+  const [err,setErr]=useState(false)
   const handle=async(e)=>{
 e.preventDefault();
     const user={
     "email":name,
     "password":password
     }
+    if(name===''||password===''){
+    setErr(true)
+    }else{
     localStorage.setItem("userreg",JSON.stringify(user))
     navigate('/quiz')
+    }
   }
 
 
@@ -30,7 +35,9 @@ e.preventDefault();
           <div className='mainbar'>
           <input type="email" className="email" value={name}  onChange={(e)=>setName(e.target.value)}/>
           <input type="text" className="password" value={password} placeholder='Any Password' onChange={(e)=>setPassword(e.target.value)} />
-          <button className='Signin' onClick={handle} disabled={name===''||password===''}>Sign in</button>
+
+          {err&&<div className='warning'>Enter the name and password!!</div>}
+          <button className='Signin' onClick={handle}>Sign in</button>
           </div>  
         </div>
     </div>
