94b6c99aa2ace34ae721622b9432f719af4d09cc
Vrashank Shetty <vrashankshetty.s@gmail.com>
2023-07-18 11:52:25 +0530

    new things

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -11,7 +11,6 @@ e.preventDefault();
     "email":name,
     "password":password
     }
-   
     localStorage.setItem("userreg",JSON.stringify(user))
     navigate('/quiz')
   }
@@ -20,6 +19,9 @@ e.preventDefault();
   
   return (
     <div className='containers'>
+      <div className='title'>
+      Hip Hop Quiz
+      </div>
         <div className='mains'>
           <div className='sidebar'>
           <span className='emailname'>Name:</span>
@@ -27,10 +29,9 @@ e.preventDefault();
           </div>
           <div className='mainbar'>
           <input type="email" className="email" value={name}  onChange={(e)=>setName(e.target.value)}/>
-          <input type="text" className="password" value={password}  onChange={(e)=>setPassword(e.target.value)} />
+          <input type="text" className="password" value={password} placeholder='Any Password' onChange={(e)=>setPassword(e.target.value)} />
           <button className='Signin' onClick={handle} disabled={name===''||password===''}>Sign in</button>
-          </div>
-           
+          </div>  
         </div>
     </div>
   )
